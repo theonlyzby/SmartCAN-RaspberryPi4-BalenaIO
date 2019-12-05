0d18df85d73d87350cfd170aa1056488e7d176c3
Theonlyzby <theonlyzby@gmail.com>
2019-12-05 17:14:16 +0100

    Update getToken.php

--- var/www/smartcan/www/getToken.php
+++ var/www/smartcan/www/getToken.php
@@ -29,37 +29,38 @@ $request = json_decode($json, true);
 //$current .= $json;
 //$current .= $request["Token"];
 
+$User = "user";
 if (isset($_COOKIE['member_login'])) {
-  //$current .= " Alias = " . $_COOKIE['member_login'];
-  //$current .= "\n Agent: " . $_SERVER['HTTP_USER_AGENT'] ."\n";
-  // Fetch DB to find user and Language
-  $sql = "SELECT * FROM `users` WHERE Alias='" . $_COOKIE['member_login'] . "';";
+  $User = $_COOKIE['member_login'];
+}
+
+//$current .= " Alias = " . $_COOKIE['member_login'];
+//$current .= "\n Agent: " . $_SERVER['HTTP_USER_AGENT'] ."\n";
+// Fetch DB to find user and Language
+$sql = "SELECT * FROM `users` WHERE Alias='" . $User . "';";
+$query = mysqli_query($DB,$sql);
+$row = mysqli_fetch_array($query, MYSQLI_BOTH);
+$ID = $row['ID'];
+$Lang = $row['Lang'];
+// Determine if already present => Update
+$sql = "SELECT COUNT(*) AS County FROM `users_notification` WHERE `Alias`='" . $User . "' AND `User_Agent`='" . $_SERVER['HTTP_USER_AGENT'] . "';";
+$query = mysqli_query($DB,$sql);
+$row = mysqli_fetch_array($query, MYSQLI_BOTH);
+if ($row['County']!="0") {
+  // Update user_notification table
+  $sql = "UPDATE `users_notification` SET `Lang` = '".$Lang."', `Token` = '".$request["Token"]."' WHERE `Alias` = '".$User."' AND `User_Agent` = '".$_SERVER['HTTP_USER_AGENT']."';";
+  //$current .= $sql . "\n";
   $query = mysqli_query($DB,$sql);
-  $row = mysqli_fetch_array($query, MYSQLI_BOTH);
-  $ID = $row['ID'];
-  $Lang = $row['Lang'];
-  // Determine if already present => Update
-  $sql = "SELECT COUNT(*) AS County FROM `users_notification` WHERE `Alias`='" . $_COOKIE['member_login'] . "' AND `User_Agent`='" . $_SERVER['HTTP_USER_AGENT'] . "';";
+} else {
+  // Create into user_notification table
+  $sql = "INSERT INTO `users_notification` (`Alias`, `Lang`, `User_Agent`, `Token`) VALUES ('".$User."', '".$Lang."', '".$_SERVER['HTTP_USER_AGENT']."', '".$request["Token"]."');";
+  //$current .= $sql . "\n";
   $query = mysqli_query($DB,$sql);
-  $row = mysqli_fetch_array($query, MYSQLI_BOTH);
-  if ($row['County']!="0") {
-    // Update user_notification table
-	$sql = "UPDATE `users_notification` SET `Lang` = '".$Lang."', `Token` = '".$request["Token"]."' WHERE `Alias` = '".$_COOKIE['member_login']."' AND `User_Agent` = '".$_SERVER['HTTP_USER_AGENT']."';";
-	//$current .= $sql . "\n";
-	$query = mysqli_query($DB,$sql);
-  } else {
-    // Create into user_notification table
-	$sql = "INSERT INTO `users_notification` (`Alias`, `Lang`, `User_Agent`, `Token`) VALUES ('".$_COOKIE['member_login']."', '".$Lang."', '".$_SERVER['HTTP_USER_AGENT']."', '".$request["Token"]."');";
-	//$current .= $sql . "\n";
-	$query = mysqli_query($DB,$sql);
-  } // END IF
-  
 } // END IF
 
 //$current .=  "\n";
 
 // Write the contents back to the file
 //file_put_contents($file, $current);
-// SELECT COUNT(*) AS County FROM `users_notification` WHERE `Alias`='" . $_COOKIE['member_login'] . " AND `User_Agent`='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'';
 
 ?>
